4656bf93fcb90bd0a552daeb3d9ef0d522b2551e
Darshan Patil <dapatil211@gmail.com>
2018-05-08 04:13:22 -0400

    Decompression file

---
+++ decompression.cpp
@@ -0,0 +1,72 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "aridecompressor.h"
+using namespace std;
+// using ht = tsl::htrie_map<char, int32_t>;
+
+void readInput(ifstream& fin, vector<char> &input){
+    int count = 0;
+    char c;
+    while(fin.get(c)){
+        input.push_back(c);
+    }
+    fin.close();
+    cout << count << endl;
+    return;
+}
+
+int main(){
+    // compress(fin);
+    // ht context;
+    // context.insert("abcd", 1);
+    // context.insert("abc", 2);
+    // context.insert("abce", 3);
+    // context.insert("abcf", 4);
+    // context.insert("abcde", 5);
+    // auto fst = context.find("abc");
+    // auto snd = context.find("abb");
+    // for(auto it = fst; it != snd; it++){
+    //     cout << it.key() << endl;
+    // }
+    // auto range = context.equal_prefix_range("abc");
+    // for(auto it = range.first; it != range.second; it++){
+    //     cout << it.key() << endl;
+    // }
+    // for(auto it = range.first; it != range.second; it++){
+    //     cout << it.key() << endl;
+    // }
+    // cout << endl;
+    // for(auto it = context.begin(); it != context.end(); it++){
+    //     cout << it.key() << endl;
+    // }
+    // cout << (string("acb") < string("abc")) << endl;
+    // cout << string("abc").back() << endl;
+
+    // ifstream fin("enwik8.comp", ios::in);
+    ifstream fin("test.comp", ios::in);
+    vector<char> input;
+    vector<char> output;
+    readInput(fin, input);
+    fin.close();
+    Model m = Model(5, 255);
+    decompress(input, output, m, 0, input.size());
+    cout << "Finished decompressing " << input.size() << endl;
+    ofstream fout("test.dec", ios::out);
+    for(uint i = 0; i < output.size(); i++){
+        fout.put(output[i]);
+    }
+    fout.close();
+    cout << "Finished Writing " << output.size() << " bytes" << endl;
+    
+    // string compress = string("bananaboat");
+    // for(auto it = compress.begin(); it != compress.end(); it++){
+    //     auto probs = m.getProbability(*it);
+    //     for(auto vit = probs.begin(); vit !=probs.end(); vit++){
+
+    //         cout << get<0>(*vit) << " " <<get<1>(*vit) << " " <<get<2>(*vit)  << endl;
+    //     }
+    //     m.updateModel(*it);
+    // }
+    return 0;
+}
